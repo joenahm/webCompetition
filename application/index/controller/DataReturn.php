f28a35052357ed35efff94d4ab2cfc6c2c657797
2867107600@qq.com <2867107600@QQ.COM>
2017-11-26 10:03:38 +0800

    小机器人的加入，存在首页登录但是兼职大厅页没有登录状态信息

--- application/index/controller/DataReturn.php
+++ application/index/controller/DataReturn.php
@@ -1,21 +1,39 @@
 <?php
 namespace app\index\controller;
 use think\Controller;
-class DateReturn extends Controller
+class Datareturn extends Controller
 {
    
     public function datareturn()
     {
-
-       $username = $_POST['username'];
-       $array = array();
-       $array['usrname'] =  $username;
-        echo  json_decode($array);
-        return $this->fetch('index/index');
-       
-   
-    
-
+           $data = array(
+               'key'=>'46c83bfdad304f9694300afa7afeb053', 
+               'info'=>"今天怎么在理工大教室里啊",
+               'userid'=>'123456',
+           );
+           $send = http_build_query($data);
+           $send;
+           $url = "http://www.tuling123.com/openapi/api";
+            $res = $this->http_request($url, $send);
+            $response = array();
+            json_decode($res,true);
+             $response =json_decode($res,true);
+            echo  $response['text'];
+    }
+    function http_request($url, $data)
+    {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);
+        if (! empty($data)) {
+            curl_setopt($curl, CURLOPT_POST, 1);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        }
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+        $output = curl_exec($curl);
+        curl_close($curl);
+        return $output;
     }
 
      
--- public/static/index/public/js/formCheck.js
+++ public/static/index/public/js/formCheck.js
@@ -206,7 +206,7 @@ function signIn(){
 			    	$("#statusBack").modal('show');
 			        $("#statusBack").find("#statusBackModalLabel").text("注册");
 					$("#statusBack").find(".glyphicon").attr("class","glyphicon glyphicon-remove");
-					$("#statusBack").find("#msgBack").text("注册失败！");
+					$("#statusBack").find("#msgBack").text("注册失败，用户名已存在！");
 			    }
 			});
 		}
