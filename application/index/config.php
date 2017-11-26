d11254f5d3ebda2d7bdbdc71150486ebb67d0576
2867107600@qq.com <2867107600@QQ.COM>
2017-11-26 13:57:29 +0800

    url

--- application/index/config.php
+++ application/index/config.php
@@ -4,8 +4,9 @@ return [
     'view_replace_str'  =>  [
         '__PUBLIC__' => SITE_URL.'/public/static',
         '__PHOTO__' =>PHOTO,
-        '__RAND__' =>'?V'.rand(),
+        '__RAND__' =>'?v'.rand(),
     ],
+    'url_route_on'  =>  false,
 
     'auto_timestamp' => true,
     'app_debug'   => true,
