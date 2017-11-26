3276d22df095664ff81d4098dc8e6228e0625a6d
2867107600@qq.com <2867107600@QQ.COM>
2017-11-26 12:27:38 +0800

    样式文件随机数

--- application/index/config.php
+++ application/index/config.php
@@ -4,6 +4,7 @@ return [
     'view_replace_str'  =>  [
         '__PUBLIC__' => SITE_URL.'/public/static',
         '__PHOTO__' =>PHOTO,
+        '__RAND__' =>'?V'.rand(),
     ],
 
     'auto_timestamp' => true,
