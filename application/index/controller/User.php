f3dd7a61754484c6d371f9424048c317db9a5f5e
2867107600@qq.com <2867107600@QQ.COM>
2017-11-27 16:13:06 +0800

    页面调转

--- application/index/controller/User.php
+++ application/index/controller/User.php
@@ -6,10 +6,16 @@ class User extends Controller
   
     public function index()
     {
-                 
+         self::userInformation();        
         return $this->fetch('User/user');
     }
-
+   public function userInformation(){
+      if(session('username')){
+          $resU = db('student')->where('username',session('username'))->find();
+          $resS = db('business')->where('username',session('username'))->find();
+     
+      }
+   }
 
 }
 
