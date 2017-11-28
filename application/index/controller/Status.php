72a9483a23b2a4d8f1c033d6f24b59fe08b25d81
Mr.L Mac <mr.l@MrLdeMacBook-Air.local>
2017-11-28 17:32:37 +0800

    详情页兼职信息获取

--- application/index/controller/Status.php
+++ application/index/controller/Status.php
@@ -7,7 +7,9 @@ class Status extends Controller
     public function index()
     {
         $status = db('information')->limit(0,1)->find();
-        var_dump($status);
+        $this->assign('status',$status);
+        $statusUser =db('business')->limit(0,1)->find();
+        $this->assign('statusUser',$statusUser);
         return $this->fetch('');
     }
 
