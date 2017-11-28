44dc843701d5e7458f71cab0df648f3ff18d8477
Mr.L Mac <mr.l@bogon.home>
2017-11-28 21:06:54 +0800

    发布信息

--- application/index/controller/InsertInformation.php
+++ application/index/controller/InsertInformation.php
@@ -1,20 +1,24 @@
 <?php
 namespace app\index\controller;
 use think\Controller;
-class InsertInformation extends Controller
+class Insertinformation extends Controller
 {
 
     public function insertInformation()
     {
-
+      $data = array();
+        if(!empty($_POST)){
             $data = [
               'title'=>$_REQUEST['title'],
               'salary'=>$_REQUEST['salary'],
               'details'=>$_REQUEST['info'],
               'insurgent'=>$_REQUEST['isurgent'],
               'cate'=>$_REQUEST['sort'],
+              'timestamp'=>time(),
             ];
-            var_dump($data);
+          }else {
+            $this->error('shibai');
+          }
           db('information')->insert($data);
         die(json_encode(array('data'=>'lmmm')));
     }
